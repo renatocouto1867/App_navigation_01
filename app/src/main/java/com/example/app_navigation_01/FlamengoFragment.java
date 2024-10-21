f5a9fbbb8128b4e0d1df345b0f2f82e11d62048b
renatocouto1867 <renatocouto1867@gmail.com>
2024-10-20 22:40:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/app_navigation_01/FlamengoFragment.java
@@ -0,0 +1,36 @@
+package com.example.app_navigation_01;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass. *
+ * create an instance of this fragment.
+ */
+public class FlamengoFragment extends Fragment {
+    TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_flamengo, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//  17:50
+
+    }
+}
